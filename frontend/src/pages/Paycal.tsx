f9615fbad881ea8a99a05386f30ec27f21ab7f23
aadsstats <aadsstats@hotmail.com>
2026-01-15 17:01:45 -0400

    Fix escaped quotes in Paycal.tsx

--- frontend/src/pages/Paycal.tsx
+++ frontend/src/pages/Paycal.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Paycal: React.FC = () => {
   return (
-    <div className=\"h-full w-full\">
+    <div className="h-full w-full">
       <iframe 
-        src=\"/paycal/paycal.html\"
-        className=\"w-full h-full border-0\"
-        title=\"Prize Pool Calculator\"
+        src="/paycal/paycal.html"
+        className="w-full h-full border-0"
+        title="Prize Pool Calculator"
         style={{ minHeight: 'calc(100vh - 64px)' }}
       />
     </div>
